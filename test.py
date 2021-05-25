08cff271f10afc9e407a8ea2fe24711ec8538652
williamwright5191 <williamwright5191@gmail.com>
2021-05-25 10:25:17 -0400

    added test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# Author: William Wright 
+# Date Created: 05/25/2021
+# Date Last Modified: 05/25/2021
+# Purpose: Simple test of python in visual studio
+
+def main():
+    value = input(str("Enter value to copy: "))
+    print(value)
+
+
+if __name__ == "__main__":
+    main()
+
