298057b6f8c2adc6e55ad7a11da769b26d7160ad
williamwright5191 <williamwright5191@gmail.com>
2021-05-25 10:26:30 -0400

    modified purpose in test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Author: William Wright 
 # Date Created: 05/25/2021
 # Date Last Modified: 05/25/2021
-# Purpose: Simple test of python in visual studio
+# Purpose: Simple test of python and git in vscode. 
 
 def main():
     value = input(str("Enter value to copy: "))
